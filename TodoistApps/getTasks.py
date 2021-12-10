10c2f54486690756ffb261f692e2a44618536f53
leekycauldron <brysonleekwen@.gmailcom>
2021-12-10 10:17:55 -0500

    Added try/catch for getting tasks as some params are optional and not always present

--- TodoistApps/getTasks.py
+++ TodoistApps/getTasks.py
@@ -8,19 +8,28 @@ from config import TODIST_API_KEY
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 headers["Authorization"] = "Bearer " + str(TODIST_API_KEY)
+
+
 #This function gets all the tasks in the "Tasks" project which is the the project to be used.
 def getTasks():
     #Get all the tasks in project and turn it into a list of dicts.
     tasks = requests.get('https://api.todoist.com/rest/v1/tasks?project_id=2208003845',headers=headers).json()
     tasksPretty = []
+    #This function tries to fetch optional params but returns empty if there is none.
+    def tryGet(temp2):
+        try:
+            return tasks[temp2]
+        except:
+            return ""
     #Filter out all the useless data
     for tasks in tasks:
         temp = {}
-        temp["Task"] = tasks["content"]
-        temp["Description"] = tasks["description"]
-        temp["Priority"] = tasks["priority"]
-        temp["Due Date"] = tasks["due"]["date"]
-        temp["ID"] = tasks["id"]
+        temp["Task"] = tryGet("content")
+        temp["Description"] = tryGet("description")
+        temp["Priority"] = tryGet("priority")
+        if tryGet("due") != "":
+            temp["Due Date"] = tasks["due"]["date"]
+        temp["ID"] = tryGet("id")
   
         tasksPretty.append(temp)
     return tasksPretty
